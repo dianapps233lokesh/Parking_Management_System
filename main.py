211791297d1d0dce97553ce78997996d0eb2a80e
lokesh <lokesh.saini@dianapps.com>
2025-05-27 17:56:29 +0530

    exception handling and manual cases handled

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.components.parking import Parking
 from src.pipeline.parking_pipeline import ParkService
 from src.components.vehicle import TwoWheeler,FourWheeler
+from src.exception.exception import ParkingManageException
+import sys
 # import time
 
 def get_vehicle_type() -> str:
@@ -17,71 +19,77 @@ def get_vehicle_type() -> str:
 
 
 def main():
-    parking=Parking()
-    park_service=ParkService(parking)
+    try:
+        parking=Parking()
+        park_service=ParkService(parking)
 
-    print("...Welcome to the Parking System...")
+        print("...Welcome to the Parking System...")
 
-    while True:
-        print("\nOptions:")
-        print("1. Allot Parking")
-        print("2. Checkout")
-        print("3. View Parked Vehicles")
-        print("4. View Available Space")
-        print("5. View Total Revenue")
-        print("6. Exit")
+        while True:
+            print("\nOptions:")
+            print("1. Allot Parking")
+            print("2. Checkout")
+            print("3. View Parked Vehicles")
+            print("4. View Available Space")
+            print("5. View Total Revenue")
+            print("6. Exit")
 
 
-        choice=input("Enter your choice: ").strip()
+            choice=input("Enter your choice: ").strip()
 
-        if choice=='1':
-            vtype=get_vehicle_type()
-            if not vtype:
-                print("Invalid choice.")
-                continue
-            vnum=input('Enter vehicle number: ').strip()
-            owner=input('Enter vehicle owner name: ').strip()
+            if choice=='1':
+                vtype=get_vehicle_type()
+                if not vtype:
+                    print("Invalid choice.")
+                    continue
+                vnum=input('Enter vehicle number: ').strip()
+                owner=input('Enter vehicle owner name: ').strip()
 
-            if vtype=='two_wheeler':
-                vehicle=TwoWheeler(vnum,owner)
-            elif vtype=='four_wheeler':
-                vehicle=FourWheeler(vnum,owner)
+                if vtype=='two_wheeler':
+                    vehicle=TwoWheeler(vnum,owner)
+                elif vtype=='four_wheeler':
+                    vehicle=FourWheeler(vnum,owner)
+                else:
+                    print("Invalid vehicle type..")
+                    continue
+
+
+                ticket_id=park_service.allot_parking(vehicle,vtype)
+                if ticket_id:
+                    print(f"Parking alloted. Ticket_id: {ticket_id}")
+                else:
+                    print("Parking full or failed")
+            
+            elif choice=='2':
+                vtype=get_vehicle_type()
+                if not vtype:
+                    print("Invalid choice.")
+                    continue
+                ticket_id=input('Enter ticket id: ').strip()
+                fee=park_service.checkout(vtype,ticket_id)
+                if not fee:
+                    print("Invalid ticket id entered")
+                    continue
+                print(f"Parking fee: {fee}")
+            
+            elif choice=='3':
+                print("Parked Vehicles:")
+                for owner, vnum in park_service.parked_vehicles():
+                    print(f"  - {owner} ({vnum})")
+
+            elif choice=='4':
+                print("Available space:",park_service.get_remaining_space())
+            
+            elif choice == "5":
+                print("Total revenue collected:", park_service.total_revenue())
+
+            elif choice == "6":
+                print("Exiting the parking system. Goodbye!")
+                break
+
             else:
-                print("Invalid vehicle type..")
-                continue
-
-
-            ticket_id=park_service.allot_parking(vehicle,vtype)
-            if ticket_id:
-                print(f"Parking alloted. Ticket_id: {ticket_id}")
-            else:
-                print("Parking full or failed")
-        
-        elif choice=='2':
-            vtype=get_vehicle_type()
-            if not vtype:
-                print("Invalid choice.")
-                continue
-            ticket_id=input('Enter ticket id: ').strip()
-            fee=park_service.checkout(vtype,ticket_id)
-            print(f"Parking fee: {fee}")
-        
-        elif choice=='3':
-            print("Parked Vehicles:")
-            for owner, vnum in park_service.parked_vehicles():
-                print(f"  - {owner} ({vnum})")
-
-        elif choice=='4':
-            print("Available space:",park_service.get_remaining_space())
-        
-        elif choice == "5":
-            print("Total revenue collected:", park_service.total_revenue())
-
-        elif choice == "6":
-            print("Exiting the parking system. Goodbye!")
-            break
-
-        else:
-            print("Invalid choice. Please try again.")
+                print("Invalid choice. Please try again.")
+    except Exception as e:
+        raise ParkingManageException(e,sys)
 
 main()
